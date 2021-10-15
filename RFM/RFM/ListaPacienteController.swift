84cceb06bb06e4fb86f2c3b85ab010f5258befb6
AlejandraCeniceros <53828096+AlejandraCeniceros@users.noreply.github.com>
2021-10-14 17:04:29 -0700

    1

---
+++ RFM/RFM/ListaPacienteController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  RFM
+//
+//  Created by Alumno on 10/14/21.
+//  Copyright © 2021 Alumno. All rights reserved.
+//
+
+import UIKit
+
+class ListaPacienteController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        self.title = "Regina Fit Meals"
+    }
+
+
+}
+
---
+++ RFM/RFM/PacienteDatosGenerales.swift
@@ -0,0 +1,20 @@
+//
+//  PacienteDatosGenerales.swift
+//  RFM
+//
+//  Created by Alumno on 10/14/21.
+//  Copyright © 2021 Alumno. All rights reserved.
+//
+
+import Foundation
+
+class PacienteDatosGenerales{
+    
+    var nombrecompleto = ""
+    var edad = ""
+    var sexo = ""
+    var estatura = ""
+    var peso = ""
+    var enfermedadalergias = ""
+
+}
