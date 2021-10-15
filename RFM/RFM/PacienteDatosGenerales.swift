79ab48594fb8a70b438c7740f756cdcf5f554b18
AlejandraCeniceros <53828096+AlejandraCeniceros@users.noreply.github.com>
2021-10-15 15:46:44 -0700

    updates15

--- RFM/RFM/PacienteDatosGenerales.swift
+++ RFM/RFM/PacienteDatosGenerales.swift
@@ -16,5 +16,23 @@ class PacienteDatosGenerales{
     var estatura = ""
     var peso = ""
     var enfermedadalergias = ""
-
+    var medidacintura = ""
+    var medidacadera = ""
+    var medidamuslo = ""
+    var medidabiceprelax = ""
+    var medidabicepflex = ""
+    
+    init(nombrecompleto: String, edad: String, sexo: String, estatura: String, peso: String, enfermedadalergias: String, medidacintura: String,medidacadera: String, medidamuslo: String, medidabiceprelax: String, medidabicepflex: String) {
+        self.nombrecompleto	= nombrecompleto
+        self.edad = edad
+        self.sexo = sexo
+        self.estatura = estatura
+        self.peso = peso
+        self.enfermedadalergias = enfermedadalergias
+        self.medidacintura = medidacintura
+        self.medidacadera = medidacadera
+        self.medidamuslo = medidamuslo
+        self.medidabiceprelax = medidabiceprelax
+        self.medidabicepflex = medidabicepflex
+    }
 }
